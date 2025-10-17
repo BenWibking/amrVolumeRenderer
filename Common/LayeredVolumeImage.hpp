d3d63b5f8375e266da347de49bdb92bc1f9c415e
Ben Wibking <ben@wibking.com>
2025-10-17 15:47:05 -0400

    remove old copyright header from new files that should not have it

--- Common/LayeredVolumeImage.hpp
+++ Common/LayeredVolumeImage.hpp
@@ -1,10 +1,6 @@
 // miniGraphics is distributed under the OSI-approved BSD 3-clause License.
 // See LICENSE.txt for details.
 //
-// Copyright (c) 2017
-// National Technology & Engineering Solutions of Sandia, LLC (NTESS). Under
-// the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains
-// certain rights in this software.
 
 #ifndef MINIGRAPHICS_LAYERED_VOLUME_IMAGE_HPP
 #define MINIGRAPHICS_LAYERED_VOLUME_IMAGE_HPP
